9af19770e4d94e05541bf43ceb5703301e2bcff6
Yaroslav Klimov <60696355+yarklimoff@users.noreply.github.com>
2020-10-17 11:25:18 +0300

    Update 3semhw1_task1.cpp

--- 3semhw1_task1.cpp
+++ 3semhw1_task1.cpp
@@ -1,13 +1,13 @@
-// https://contest.yandex.ru/contest/19772/run-report/35141594/
+// https://contest.yandex.ru/contest/19772/run-report/36816736/
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include "string"
+#include <string>
 
-std::vector<int> prefix_function (const std::string& s) {
+std::vector<int> PrefixFunction (const std::string& s) {
     int n = (int) s.length();
     std::vector<int> pi (n, 0);
-    for (int i = 1; i < n; ++i) {
+    for (size_t i = 1; i < n; ++i) {
         int k = pi[i - 1];
         while (k > 0 && s[i] != s[k])
             k = pi[k - 1];
@@ -18,16 +18,28 @@ std::vector<int> prefix_function (const std::string& s) {
     return pi;
 }
 
+void FindInclusion(const std::string& s, const std::string& pattern){
+    std::vector<int> prefix_f_pattern = PrefixFunction(pattern);
+    int last_prefix = 0;
+    for (size_t i = 0; i < s.length(); ++i) {
+        while (last_prefix > 0 && pattern[last_prefix] != s[i]){
+            last_prefix = prefix_f_pattern[last_prefix - 1];
+        }
+     
+        if (pattern[last_prefix] == s[i]) {
+            ++last_prefix;
+        }
+     
+        if (last_prefix == pattern.length()) {
+            std::cout << i + 1 - pattern.length() << " ";
+            }
+        }
+}
 
 
 int main() {
     std::string s, pattern;
     std::cin >> pattern >> s;
-    s = pattern + "$" + s;
-    std::vector<int> result = prefix_function(s);
-    for (size_t i = 0; i < s.length(); ++i) {
-        if (result[i] == pattern.length())
-            std::cout << i - 2 * pattern.length() << " ";
-    }
+    FindInclusion(s, pattern);
     return 0;
 }
