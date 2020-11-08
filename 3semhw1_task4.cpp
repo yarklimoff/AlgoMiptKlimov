20876dc7235e7bdbbcb3ab4ac38239c0378efe4a
Yaroslav Klimov <60696355+yarklimoff@users.noreply.github.com>
2020-11-08 21:34:57 +0300

    Update 3semhw1_task4.cpp

--- 3semhw1_task4.cpp
+++ 3semhw1_task4.cpp
@@ -1,16 +1,15 @@
-// https://contest.yandex.ru/contest/20019/run-report/36993813/
+// https://contest.yandex.ru/contest/20019/run-report/39565032/
 #include <iostream>
 #include <vector>
 #include <numeric>
 #include <string>
 #include <functional>
-#define alphabet 27
+#define alphabet 123
 
-std::vector<int> BuildSuffMass(const std::string& s) {
+void FirstStep(std::vector<int> &cnt, const std::string& s,
+               std::vector<int> &class_id, std::vector<int> &suf_array,
+               int &classes) {
     size_t n = s.length();
-    int classes = 1;
-    std::vector<int> cnt(alphabet, 0), suf_mass(n, 0), class_id(n, 0);
-    std::vector<int> temp_suf_mass(n, 0), temp_classes(n, 0);
     for (int i = 0; i < n; ++i) {
         ++cnt[s[i]];
     }
@@ -18,62 +17,80 @@ std::vector<int> BuildSuffMass(const std::string& s) {
         cnt[i] += cnt[i - 1];
     }
     for (int i = 0; i < n; ++i) {
-        suf_mass[--cnt[s[i]]] = i;
+        suf_array[--cnt[s[i]]] = i;
     }
     for (int i = 1; i < n; ++i) {
-        class_id[suf_mass[i]] = class_id[suf_mass[i - 1]];
-        if (s[suf_mass[i]] != s[suf_mass[i - 1]]) {
-            ++class_id[suf_mass[i]];
+        class_id[suf_array[i]] = class_id[suf_array[i - 1]];
+        if (s[suf_array[i]] != s[suf_array[i - 1]]) {
+            ++class_id[suf_array[i]];
             ++classes;
         }
     }
+}
+
+void OtherSteps(std::vector<int> &cnt, const std::string& s,
+                std::vector<int> &class_id, std::vector<int> &suf_array,
+                int &classes) {
+    size_t n = s.length();
+    std::vector<int> temp_suf_array(n, 0), temp_classes(n, 0);
     for (int len = 1; len < n; len *= 2) {
         for (int i = 0; i < n; ++i) {
-            temp_suf_mass[i] = suf_mass[i] - len;
-            if (temp_suf_mass[i] < 0) {
-                temp_suf_mass[i] += n;
+            temp_suf_array[i] = suf_array[i] - len;
+            if (temp_suf_array[i] < 0) {
+                temp_suf_array[i] += n;
             }
         }
         cnt = std::vector<int>(classes, 0);
         for (int i = 0; i < n; ++i) {
-            ++cnt[class_id[temp_suf_mass[i]]];
+            ++cnt[class_id[temp_suf_array[i]]];
         }
         for (int i = 1; i < classes; ++i) {
             cnt[i] += cnt[i - 1];
         }
         for (ssize_t i = n - 1; i >= 0; --i) {
-            suf_mass[--cnt[class_id[temp_suf_mass[i]]]] = temp_suf_mass[i];
+            suf_array[--cnt[class_id[temp_suf_array[i]]]] = temp_suf_array[i];
         }
-        temp_classes[temp_suf_mass[0]] = 0;
+        temp_classes[temp_suf_array[0]] = 0;
         classes = 1;
         for (int i = 1; i < n; ++i) {
-            int mid1 = (suf_mass[i] + len) % n,  mid2 = (suf_mass[i - 1] + len) % n;
-            if (class_id[suf_mass[i]] != class_id[suf_mass[i-1]] || class_id[mid1] != class_id[mid2]) {
+            int mid1 = (suf_array[i] + len) % n,  mid2 = (suf_array[i - 1] + len) % n;
+            if (class_id[suf_array[i]] != class_id[suf_array[i - 1]] ||
+                class_id[mid1] != class_id[mid2]) {
                 ++classes;
             }
-            temp_classes[suf_mass[i]] = classes - 1;
+            temp_classes[suf_array[i]] = classes - 1;
         }
         class_id = temp_classes;
     }
-    return suf_mass;
 }
 
-std::vector<int> buildLCP(std::vector<int>& suf_mass, const std::string& s) {
+std::vector<int> BuildSuffArray(const std::string& s) {
     size_t n = s.length();
-    std::vector<int> rev_suf_mass(n, 0);
+    int classes = 1;
+    std::vector<int> cnt(alphabet, 0), suf_array(n, 0), class_id(n, 0);
+    FirstStep(cnt, s, class_id, suf_array, classes);
+    OtherSteps(cnt, s, class_id, suf_array, classes);
+    return suf_array;
+}
+
+
+
+std::vector<int> BuildLCP(std::vector<int>& suf_array, const std::string& s) {
+    size_t n = s.length();
+    std::vector<int> rev_suf_array(n, 0);
     std::vector<int> LCP(n, 0);
     for (int i = 0; i < n; ++i) {
-        rev_suf_mass[suf_mass[i]] = i;
+        rev_suf_array[suf_array[i]] = i;
     }
     int curLCP = 0;
     for (int i = 0; i < n; ++i) {
-        int p = rev_suf_mass[i];
+        int p = rev_suf_array[i];
         if (p == n - 1) {
             LCP[p] = 0;
             curLCP = 0;
             continue;
         }
-        int q = suf_mass[p + 1];
+        int q = suf_array[p + 1];
         --curLCP;
         if (curLCP < 0) {
             curLCP = 0;
@@ -86,20 +103,20 @@ std::vector<int> buildLCP(std::vector<int>& suf_mass, const std::string& s) {
     return LCP;
 }
 
+void Solve(std::string& s) {
+    std::vector<int> suff_array, LCP;
+    s += '\0';
+    size_t n = s.length();
+    suff_array = BuildSuffArray(s);
+    LCP = BuildLCP(suff_array, s);
+    long long ans = n * n - accumulate(suff_array.begin(), suff_array.end(), 0)
+                    - std::accumulate(LCP.begin(), LCP.end(), 0) - n;
+    std::cout << ans;
+}
+
 int main() {
     std::string s;
     std::cin >> s;
-    std::vector<int> suff_mass, LCP;
-    s += '`'; // ascii: '`' - 96, 'a' - 97
-    for (int i = 0; i < s.length(); ++i) {
-        s[i] -= '`';
-    }
-    size_t n = s.length();
-    suff_mass = BuildSuffMass(s);
-    LCP = buildLCP(suff_mass, s);
-    long long ans = accumulate(suff_mass.begin(), suff_mass.end(), 0);
-    ans = n * n - ans;
-    ans -= std::accumulate(LCP.begin(), LCP.end(), 0);
-    std::cout << ans - n;
+    Solve(s);
     return 0;
 }
