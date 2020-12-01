ff2cb9cda8eacb639b1a2c010f15501ba83987f0
Yaroslav Klimov <60696355+yarklimoff@users.noreply.github.com>
2020-12-01 20:44:32 +0300

    Update 3semhw1_task4.cpp

--- 3semhw1_task4.cpp
+++ 3semhw1_task4.cpp
@@ -1,39 +1,30 @@
-// https://contest.yandex.ru/contest/20019/run-report/39565032/
+// https://contest.yandex.ru/contest/20019/run-report/43269142/
 #include <iostream>
 #include <vector>
 #include <numeric>
-#include <string>
 #include <functional>
 #define alphabet 123
 
-void FirstStep(std::vector<int> &cnt, const std::string& s,
-               std::vector<int> &class_id, std::vector<int> &suf_array,
-               int &classes) {
-    size_t n = s.length();
+void Count(std::vector<int> &cnt, const std::string& s,
+           std::vector<int> &class_id, std::vector<int> &suf_array,
+           int &classes, std::vector<int>& temp_suf_array) {
+    size_t n = s.size();
     for (int i = 0; i < n; ++i) {
-        ++cnt[s[i]];
+        ++cnt[class_id[temp_suf_array[i]]];
     }
-    for (int i = 1; i < alphabet; ++i) {
+    for (int i = 1; i < classes; ++i) {
         cnt[i] += cnt[i - 1];
     }
-    for (int i = 0; i < n; ++i) {
-        suf_array[--cnt[s[i]]] = i;
-    }
-    for (int i = 1; i < n; ++i) {
-        class_id[suf_array[i]] = class_id[suf_array[i - 1]];
-        if (s[suf_array[i]] != s[suf_array[i - 1]]) {
-            ++class_id[suf_array[i]];
-            ++classes;
-        }
+    for (ssize_t i = n - 1; i >= 0; --i) {
+        suf_array[--cnt[class_id[temp_suf_array[i]]]] = temp_suf_array[i];
     }
 }
 
-void OtherSteps(std::vector<int> &cnt, const std::string& s,
-                std::vector<int> &class_id, std::vector<int> &suf_array,
-                int &classes) {
+void Step(std::vector<int> &cnt, const std::string& s,
+           std::vector<int> &class_id, std::vector<int> &suf_array,
+           int &classes, std::vector<int>& temp_suf_array, std::vector<int>& temp_classes) {
     size_t n = s.length();
-    std::vector<int> temp_suf_array(n, 0), temp_classes(n, 0);
-    for (int len = 1; len < n; len *= 2) {
+    for (int len = 0; len < n; len = ((len == 0) ? 1 : 2 * len)) {
         for (int i = 0; i < n; ++i) {
             temp_suf_array[i] = suf_array[i] - len;
             if (temp_suf_array[i] < 0) {
@@ -41,24 +32,17 @@ void OtherSteps(std::vector<int> &cnt, const std::string& s,
             }
         }
         cnt = std::vector<int>(classes, 0);
-        for (int i = 0; i < n; ++i) {
-            ++cnt[class_id[temp_suf_array[i]]];
-        }
-        for (int i = 1; i < classes; ++i) {
-            cnt[i] += cnt[i - 1];
-        }
-        for (ssize_t i = n - 1; i >= 0; --i) {
-            suf_array[--cnt[class_id[temp_suf_array[i]]]] = temp_suf_array[i];
-        }
+        Count(cnt, s, class_id, suf_array, classes, temp_suf_array);
         temp_classes[temp_suf_array[0]] = 0;
         classes = 1;
         for (int i = 1; i < n; ++i) {
             int mid1 = (suf_array[i] + len) % n,  mid2 = (suf_array[i - 1] + len) % n;
+            temp_classes[suf_array[i]] = temp_classes[suf_array[i - 1]];
             if (class_id[suf_array[i]] != class_id[suf_array[i - 1]] ||
                 class_id[mid1] != class_id[mid2]) {
+                ++temp_classes[suf_array[i]];
                 ++classes;
             }
-            temp_classes[suf_array[i]] = classes - 1;
         }
         class_id = temp_classes;
     }
@@ -66,10 +50,15 @@ void OtherSteps(std::vector<int> &cnt, const std::string& s,
 
 std::vector<int> BuildSuffArray(const std::string& s) {
     size_t n = s.length();
-    int classes = 1;
+    int classes = alphabet;
     std::vector<int> cnt(alphabet, 0), suf_array(n, 0), class_id(n, 0);
-    FirstStep(cnt, s, class_id, suf_array, classes);
-    OtherSteps(cnt, s, class_id, suf_array, classes);
+    std::vector<int> temp_suf_array(n, 0), temp_classes(n, 0);
+    for (int i = 0; i < n; ++i) {
+        temp_suf_array[i] = i;
+        suf_array[i] = i;
+        class_id[i] = s[i];
+    }
+    Step(cnt, s, class_id, suf_array, classes, temp_suf_array, temp_classes);
     return suf_array;
 }
 
