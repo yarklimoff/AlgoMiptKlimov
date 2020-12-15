1e788d262909c460f4a5174c83a5868759276836
Yaroslav Klimov <60696355+yarklimoff@users.noreply.github.com>
2020-12-16 01:09:47 +0300

    Update 3semhw3_taskC.cpp

--- 3semhw3_taskC.cpp
+++ 3semhw3_taskC.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
 
-int Mex(vector <int>& to_mex) {
+int Mex(std::vector <int>& to_mex) {
     int i = 0;
     while (find(to_mex.begin(), to_mex.end(), i)!=to_mex.end()) {
         ++i;
@@ -11,11 +10,11 @@ int Mex(vector <int>& to_mex) {
     return i;
 }
 
-int SpragueGrandy(int n, vector<int>& sg) {
+int SpragueGrandy(int n, std::vector<int>& sg) {
     if (sg[n] != -1) {
         return sg[n];
     } else {
-        vector<int> to_mex;
+        std::vector<int> to_mex;
         to_mex.push_back(SpragueGrandy(n - 1, sg));
         for (int i = 1; i <= n - 2; ++i) {
             to_mex.push_back(SpragueGrandy(i, sg) ^ SpragueGrandy(n - i - 1, sg));
@@ -25,25 +24,26 @@ int SpragueGrandy(int n, vector<int>& sg) {
     }
 }
 
-void WinSchtirlitz(int n, vector<int>& sg) {
+void WinSchtirlitz(int n, std::vector<int>& sg) {
     for (int i = 1; i <= n; ++i) {
-        if ((sg[i-1] ^ sg[n-i]) == 0) {
-            cout << i << endl;
+        if ((sg[i-1]^sg[n-i]) == 0) {
+            std::cout << i << std::endl;
         }
     }
 }
 int main() {
     int n = 0;
-    cin >> n;
-    vector<int> sg(n+1, -1);
+    std::cin >> n;
+    std::vector<int> sg(n+1, -1);
     sg[0] = 0;
     sg[1] = 0;
     sg[2] = 1;
     if (SpragueGrandy(n, sg) > 0) {
-        cout << "Schtirlitz\n";
+        std::cout << "Schtirlitz\n";
         WinSchtirlitz(n, sg);
+
     } else {
-        cout << "Mueller\n";
+        std::cout << "Mueller\n";
     }
     return 0;
 }
